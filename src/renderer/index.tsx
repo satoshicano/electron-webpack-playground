f2e0b8cdcd6338afb628ec3c2be22eaf6c52d664
satoshicano <satoshicano@gmail.com>
2019-01-02 02:36:00 +0900

    install react typescript

---
+++ src/renderer/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const Header = <h1>hello electron webpack</h1>;
+
+ReactDOM.render(Header, document.getElementById("app"));
